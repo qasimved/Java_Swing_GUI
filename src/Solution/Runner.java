72702f5dab19e0ec60b4be76e023abc4a4949af2
qasimved <qasimved@gmail.com>
2020-10-18 13:03:29 +0200

    initial commit

---
+++ src/Solution/Runner.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Solution;
+
+/**
+ *
+ * @author AKIFHASAN
+ */
+public class Runner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        new Activity_1().setVisible(true);
+//        new Activity_2().setVisible(true);
+//        new Activity_3().setVisible(true);
+//        new Activity_4().setVisible(true);
+//          new Activity_5().setVisible(true);
+         new Activity_6().setVisible(true);
+    }   
+}
